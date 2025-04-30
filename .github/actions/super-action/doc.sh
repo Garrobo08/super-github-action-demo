e17e8e81015d878b98312ffd578c6215988433ee
Garrobo08 <andreagarroboguzman@gmail.com>
2025-04-30 11:21:03 +0200

    doc

--- .github/actions/super-action/doc.sh
+++ .github/actions/super-action/doc.sh
@@ -7,7 +7,7 @@ echo "📄 Generando documentación con JSDoc..."
 ./node_modules/.bin/jsdoc -c jsdoc.json 
 
 echo "📁 Contenido antes de generar documentación:"
-ls -R
+tree
 
 echo "📁 Contenido después de JSDoc:"
 ls -R out || echo "❌ No se generó el directorio 'out'"
