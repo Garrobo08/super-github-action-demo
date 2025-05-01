6cd552f71de5a8dc96fd5f02b88adc210f9327dd
Jordigb44 <jordigb44@gmail.com>
2025-05-01 13:15:20 +0200

    add rest funtion

--- src/example.js
+++ src/example.js
@@ -5,3 +5,12 @@
 function sumar(a, b) {
   return a + b;
 }
+
+
+/** Resta dos números.
+@param {number} a Primer número
+@param {number} b Segundo número
+@returns {number} La suma de a y b */
+function restar(a, b) {
+  return a - b;
+}
