b1df817d4fc0486db7c3193991a3ae8b186f51c8
Jordigb44 <jordigb44@gmail.com>
2025-05-02 14:01:19 +0200

    📝 Corregir comentarios en la función restar

--- src/example.js
+++ src/example.js
@@ -6,18 +6,10 @@ function sumar(a, b) {
   return a + b;
 }
 
-/** Resta dos números. 
- @param {number} a Primer número
- @param {number} b Segundo número
- @returns {number} La suma de a y b */
-function restar(a, b) {
-  return a - b;
-}
-
-/** Multiplica dos números.
-@param {number} a Primer número
-@param {number} b Segundo número
-@returns {number} El resultado de multiplicar a y b */
-function multiplicar(a, b) {
-  return a * b;
-}
+/** Resta dos números.
+// @param {number} a Primer número
+// @param {number} b Segundo número
+// @returns {number} La suma de a y b */
+// function restar(a, b) {
+//   return a - b;
+// } 
